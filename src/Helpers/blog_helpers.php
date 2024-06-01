4a9934acfc6d9e7d9fd9374d162e5bf8a78d654e
nzogo <boss.sm11@gmail.com>
2024-06-01 17:33:46 +0300

    add method to get post

--- src/Helpers/blog_helpers.php
+++ src/Helpers/blog_helpers.php
@@ -4,3 +4,8 @@ function getLayoutSections()
 {
     return \Nisimpo\Blog\Models\Category::with('categories')->where('name','Layout')->get()->first();
 }
+
+function getPostBySlug($slug)
+{
+    return \Nisimpo\Blog\Models\Post::where('slug',$slug)->get()->first();
+}
