c8fc0be11d9d8ab411e66beddffb025a4b4c7671
nzogo <boss.sm11@gmail.com>
2024-06-02 11:10:13 +0300

    get post by category name

--- src/Helpers/blog_helpers.php
+++ src/Helpers/blog_helpers.php
@@ -10,6 +10,11 @@ function getPostBySlug($slug)
     return \Nisimpo\Blog\Models\Post::where('slug',$slug)->get()->first();
 }
 
+function getPostByCategory($categoryName)
+{
+    return \Nisimpo\Blog\Models\Category::with('posts')->where('name',$categoryName)->get()->first()->posts;
+}
+
 function getLatestPostList($limit = 3){
     return \Nisimpo\Blog\Models\Post::orderBy('id','desc')->limit($limit)->get();
 }
