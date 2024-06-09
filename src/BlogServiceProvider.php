e7e95819d98a1d989df34f24e1e58e4b45ee3c90
nzogo <boss.sm11@gmail.com>
2024-06-10 00:42:12 +0300

    fix bugs and enhancement

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -24,6 +24,8 @@ class BlogServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'blog');
+
+        $this->publishAll();
     }
 
     private function publishAll(): void
