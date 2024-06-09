5cdb8ee8d0756eaee1eddbba499b617bca86417b
nzogo <boss.sm11@gmail.com>
2024-06-10 00:16:24 +0300

    allow files publishing views route controllers and migrations

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -2,12 +2,20 @@
 
 namespace Nisimpo\Blog;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Symfony\Component\Finder\SplFileInfo;
 
 class BlogServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallationCommand::class
+            ]);
+        }
         $this->mergeConfigFrom(__DIR__.'/../config/livewire.php','blog');
     }
 
@@ -17,4 +25,14 @@ class BlogServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'blog');
     }
+
+    private function publishAll(): void
+    {
+        $this->publishes([
+            __DIR__.'/resources/views/categories' => base_path('resources/views/blog'),
+            __DIR__.'/resources/views/posts' => base_path('resources/views/blog'),
+            __DIR__.'/resources/views/settings' => base_path('resources/views/blog'),
+        ],'blog');
+    }
+
 }
---
+++ src/InstallationCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Nisimpo\Blog;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Finder\SplFileInfo;
+
+#[AsCommand(name: 'blog:install')]
+class InstallationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'blog:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scaffold the blog files';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->components->info('Preparing for blog installation');
+
+        if (! is_dir($directory = app_path('Http/Controllers/Blog'))) {
+            mkdir($directory, 0755, true);
+        }
+
+        $this->components->info('Copying blog controllers files');
+        $filesystem = new Filesystem;
+
+        collect($filesystem->allFiles(__DIR__.'/Controllers/stubs'))
+            ->each(function (SplFileInfo $file) use ($filesystem) {
+                $filesystem->copy(
+                    $file->getPathname(),
+                    app_path('Http/Controllers/Blog/'.Str::replaceLast('.stub', '.php', $file->getFilename()))
+                );
+            });
+
+        $this->components->info('Copying blog routes to web.php  route file');
+        file_put_contents(
+            base_path('routes/web.php'),
+            file_get_contents(__DIR__.'/routes/routes.stub'),
+            FILE_APPEND
+        );
+
+        $this->components->info('Publishing blog views and migrations');
+        $this->call("vendor:publish",[
+            '--tag' => 'blog',
+            '--force' => true,
+        ]);
+
+        $this->components->info('Blog files scaffolding generated successfully.');
+    }
+}
