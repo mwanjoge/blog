06f5992fd755f81354a23d18547ce52587cf7011
nzogo <boss.sm11@gmail.com>
2024-06-20 00:02:57 +0300

    adding media to settings

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -5,9 +5,12 @@ namespace Nisimpo\Blog\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Setting extends Model
+class Setting extends Model implements HasMedia
 {
+    use InteractsWithMedia;
     protected $guarded = [];
 
     public function settings(): HasMany
